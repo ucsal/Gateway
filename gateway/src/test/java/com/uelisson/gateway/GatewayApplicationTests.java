4b65860b5fa06e61d883d19328a1388c6dbe3c0c
Uelisson <tagots09@gmail.com>
2025-05-30 14:28:18 -0300

    first commit

---
+++ gateway/src/test/java/com/uelisson/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.uelisson.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
